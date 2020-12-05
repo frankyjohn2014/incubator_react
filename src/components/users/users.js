50f21417b5ccb09e26cefe405d8d383489aefa97
frankyjohn <frankyjohn2014@gmail.com>
2020-12-05 11:21:07 +0300

    lesson 51 axios api

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import styles from './users.module.css'
+import * as axios from 'axios'
+
 
 const Users = (props) => {
     if (props.users.users.length ===0) {
-        props.setUsers([
-            {id:1, followed: true,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Dmitriy!", status:'Python', location:{city:'Minsk',country:'Belarus'}},
-            {id:2, followed: false,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Vasya!", status:'Ruby', location:{city:'Kiev',country:'Ukraine'}},
-            {id:3, followed: true,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Sergey!", status:'JavaScript', location:{city:'Moskow',country:'Russia'}},
-        ]
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+            props.setUsers(response.data.items)
+        }
         )
     }
-
+    // {id:1, followed: true,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Dmitriy!", status:'Python', location:{city:'Minsk',country:'Belarus'}},
+    // {id:2, followed: false,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Vasya!", status:'Ruby', location:{city:'Kiev',country:'Ukraine'}},
+    // {id:3, followed: true,photoUrl:'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg', name:"Sergey!", status:'JavaScript', location:{city:'Moskow',country:'Russia'}},
     return <div> 
         {
         props.users.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : 'https://www.kinonews.ru/insimgs/2019/newsimg/newsimg87089.jpg'} className={styles.userPhoto}/>
                 </div>
                 <div>
                     {u.followed ?  
@@ -30,10 +33,10 @@ const Users = (props) => {
                         <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
-                    <span>
+                    {/* <span>
                         <div>{u.location.country}</div>
                         <div>{u.location.city}</div>
-                    </span>
+                    </span> */}
                 </span>
             </span>
         </div>)}
