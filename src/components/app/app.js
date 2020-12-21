9e3c38f1bc55a9f96eb8fa32a4f99078f6e89dcc
franky <frankyjohn2014@gmail.com>
2020-12-21 02:17:15 -0500

    les 94 lazy suspend

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,15 +4,16 @@ import Navbar from '../navbar/navbar';
 import PostsContainer from '../posts/postsContainer';
 import { BrowserRouter, Route } from 'react-router-dom';
 import DialogsContainer from '../dialogs/dialogsContainer';
-import UsersContainer from '../users/usersContainer';
-import ProfileContainer from '../profile/profileContainer';
+// import UsersContainer from '../users/usersContainer';
+// import ProfileContainer  from '../profile/profileContainer';
 import AuthContainer from '../auth/authContainer';
 import { compose } from 'redux';
 import { initializeThunk } from '../app/appReducers'
 import { connect } from 'react-redux'
 import { withRouter } from "react-router"; 
 import Spinner from '../common/spinner/spinner';
-
+const UsersContainer = React.lazy(() => import('../users/usersContainer'));
+const ProfileContainer = React.lazy(() => import( '../profile/profileContainer'));
 class App extends React.Component {
     componentDidMount() {
          this.props.initializeThunk()
@@ -22,19 +23,19 @@ class App extends React.Component {
             return <Spinner/>
         }
         return (
-            <BrowserRouter>
                 <div>
-                    <Navbar/>
-                    <Header/>
-                    <div className="app-wrapper-content"> 
-                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                        <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-                        <Route path="/posts" render={() => <PostsContainer/>}/>
-                        <Route path="/users" render={() => <UsersContainer/>}/>
-                        <Route path="/login" render={() => <AuthContainer/>}/>
-                    </div>
+                    
+                        <Navbar/>
+                        <Header/>
+                        <div className="app-wrapper-content"> 
+                            <Route path="/dialogs" render={() => {return <React.Suspense fallback={<Spinner/>}><DialogsContainer/></React.Suspense>}}/>
+                            <Route path="/profile/:userId?" render={() => {return<React.Suspense fallback={<Spinner/>}><ProfileContainer/></React.Suspense>}}/>
+                            <Route path="/posts" render={() => <PostsContainer/>}/>
+                            <Route path="/users" render={() => <UsersContainer/>}/>
+                            <Route path="/login" render={() => <AuthContainer/>}/>
+                        </div>
+
                 </div>
-            </BrowserRouter>
         )
     }
 }
